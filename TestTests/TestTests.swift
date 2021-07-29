e844e49d48aad561f3037e8c2ff41060d4ce9794
ahmad-00 <mohammadi.ahmad00@gmail.com>
2021-07-29 22:23:08 +0300

    Sample test added

--- TestTests/TestTests.swift
+++ TestTests/TestTests.swift
@@ -18,16 +18,52 @@ class TestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCalculator() throws {
+        let vm = CalendarVM()
+        guard let holidays = getHolidaysFromTestData() else {
+            XCTAssertTrue(false, "Unable to load holidays json data")
         }
+        vm.holidays = holidays
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let startDate = formatter.date(from: "2021/10/01")!
+        let endDate = formatter.date(from: "2021/10/15")!
+        
+        vm.setStartDate(startDate)
+        vm.setEndDate(endDate)
+        
+        XCTAssertEqual(vm.result, "Number of Bussiness Days: 8")
+        
     }
+    
+    private func getHolidaysFromTestData() -> Holidays?{
+        
+        guard let path = Bundle.main.path(forResource: "holidays", ofType: "json") else {
+            return nil
+        }
+        guard let jsonData = try? String(contentsOfFile: path).data(using: .utf8) else {
+                return nil
+        }
+        
+        guard let decodedData = try? JSONDecoder().decode(Holidays.self, from: jsonData) else {
+            return nil
+        }
+        
+        return decodedData
+        
+    }
+    
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
